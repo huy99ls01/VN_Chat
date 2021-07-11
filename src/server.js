7b8ce7678034dd47d503c92862a4c6d65e4c4fe2
huy99ls01 <huy99ls01@gmail.com>
2021-07-12 00:50:26 +0700

    [1.0.0] add routes and controllers

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import ConnectDB from "./config/connectDB";
 //import ContactModel from "./models/contact.model";
 import configViewEngine from "./config/viewEngine";
-
+import initRoutes from "./routes/web";
 
 // Init app
 let app = express();
@@ -13,13 +13,8 @@ ConnectDB();
 // Config view Engine
 configViewEngine(app);
 
-app.get("/", (req, res) => { 
-    return res.render("main/master");
-});
-
-app.get("/login-register", (req, res) => { 
-    return res.render("auth/loginRegister");
-});
+// Init all routes
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
     console.log(`Hello, Running at ${ process.env.APP_HOST }:${ process.env.APP_PORT }/`);
