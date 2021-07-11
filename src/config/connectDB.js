d012e195dbf7950c20be12db7914975afd95664f
huy99ls01 <huy99ls01@gmail.com>
2021-07-12 00:22:51 +0700

    [1.0.0] Add static pages.

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -8,12 +8,12 @@ import bluebird from "bluebird";
 let connectDB = () => { 
     mongoose.Promise = bluebird;
 
-    let DB_CONNECTION = "mongodb";
-    let DB_HOST = "localhost";
-    let DB_PORT = 27017;
-    let DB_NAME = "VN_Chat";
-    let DB_USERNAME = "";
-    let DB_PASSWORD = "";
+    //let DB_CONNECTION = "mongodb";
+    //let DB_HOST = "localhost";
+    //let DB_PORT = 27017;
+    //let DB_NAME = "VN_Chat";
+    //let DB_USERNAME = "";
+    //let DB_PASSWORD = "";
     //  mongodb://localhost:20717/VN_Chat
     let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
