daa4ab0f4f4723a751fb760b643fb8ce5fcd3f6c
huyTQ <huy99ls01@gmail.com>
2021-07-15 12:38:59 +0700

    [1.0.0] Finish register user

--- lang/vi.js
+++ lang/vi.js
@@ -4,3 +4,16 @@ export const transValidation = {
     password_incorrect: "Mat khau phai chua it nhat 8 ky tu, bao gom cho hoa, chu thuong, chu so va ky tu",
     password_confirmation_incorrect: "mat khau khong giong nhau",
 }
+
+
+export const transErrors = {
+    account_in_use: "Email nay da duoc su dung",
+    account_removed: "Account nay da bi go khoi he thong",
+    account_not_active: "Account da duoc tao nhung chua active"
+}
+
+export const transSuccess = {
+    userCreated: (userEmail) => {
+        return `Account <strong>${userEmail}</strong> da duoc tao`;
+    }
+}
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,42 @@
-import e from "express";
 import {validationResult} from "express-validator/check";
+import {auth} from "./../services/index";
 
 let getLoginRegister = (req, res) => { 
-    return res.render("auth/master");
+    return res.render("auth/master", {
+        errors: req.flash("errors"),
+        success: req.flash("success")
+    });
 };  
 
-let postRegister = (req, rex) => { 
+let postRegister = async (req, res) => { 
     let errorArr = [];
+    let successArr = [];
+
     let validationErrors = validationResult(req);
     if (!validationResult(req).isEmpty()) {
         let errors = Object.values(validationErrors.mapped())
         errors.forEach(item => {
             errorArr.push(item.msg)
         });
-        console.log(errorArr)
-        return;
+        //console.log(errorArr)
+
+        req.flash("errors", errorArr);
+        return res.redirect("/login-register");
     }
 
-    console.log(req.body)
+    try {
+        let createUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password);
+        successArr.push(createUserSuccess);
+        
+        req.flash("success", successArr);   
+        return res.redirect("/login-register");
+    } catch (error) {
+        errorArr.push(error)
+
+        req.flash("errors", errorArr);
+        return res.redirect("/login-register");
+    }
+    
 };
 
 module.exports = {
